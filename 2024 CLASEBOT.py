8c955d877c49f246ed034112a64d374d4a57e7c4
gabopilot22 <giron.gabriel1712@gmail.com>
2024-08-07 20:10:03 -0500

    Add files via upload

---
+++ 2024 CLASEBOT.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run("MTI2ODM3Mjg4NjMzNTUyNDg5Ng.GMKSeq.ngP7x2tSw4kx5Yv4JOTovViuQM2GPfG5DJwzA0")
