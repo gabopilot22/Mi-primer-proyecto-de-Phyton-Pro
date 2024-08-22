0fb6c91d355477db3b8605afa3742001797baa4d
gabopilot22 <giron.gabriel1712@gmail.com>
2024-08-21 19:15:18 -0500

    Update 2024 CLASEBOT.py

--- 2024 CLASEBOT.py
+++ 2024 CLASEBOT.py
@@ -1,4 +1,7 @@
+import random
 import discord
+import os
+import requests
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -18,9 +21,32 @@ async def hello(ctx):
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+    
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir("images"))
+    with open(f'images/{img_name}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
 @bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
 
-bot.run("MTI2ODM3Mjg4NjMzNTUyNDg5Ng.GMKSeq.ngP7x2tSw4kx5Yv4JOTovViuQM2GPfG5DJwzA0")
+bot.run("MTI2ODM3Mjg4NjMzNTUyNDg5Ng.GWxk2x.hpi9GE2DQ8LSM5ioJU7-BLBf3qDgt8zRAGoCx8")
